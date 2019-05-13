fb55d23d7d6c22af55bb1fca58b057abaad4b666
xhskedu <xhskedu@qq.com>
2019-05-13 21:17:40 +0800

    czb

---
+++ src/test/java/com/software/test/generatingexam/serviceTest/QuestionServiceTest.java
@@ -0,0 +1,19 @@
+package com.software.test.generatingexam.serviceTest;
+
+import com.software.test.generatingexam.baseTest.SpringTestCase;
+import com.software.test.generatingexam.service.QuestionService;
+import com.software.test.generatingexam.service.UserService;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class QuestionServiceTest extends SpringTestCase {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private QuestionService questionService;
+
+    @Test
+    private
+}
